f47daad75084b2c511b645316e9339b08154b626
cquinterop <cristianqpineda@gmail.com>
2025-08-15 21:07:35 +0200

    feat: add PandaCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { css } from "../styled-system/css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className={css({
+          minHeight: "100vh",
+          backgroundColor: "blue.100",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        })}
+      >
+        <div
+          className={css({
+            width: 400,
+            height: 400,
+            bg: "blue.200",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: "3xl",
+            padding: "10px 15px",
+          })}
+        >
+          <h1
+            className={css({
+              fontSize: "3xl",
+              fontWeight: "bold",
+              color: "blue.800",
+            })}
+          >
+            Hello Panda CSS
+          </h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
